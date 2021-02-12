1c660a3fab348e03b791d11582a67032702ed47f
Julio Melo <julio.melo@imd.ufrn.br>
2021-02-12 13:04:14 -0300

    Adding example

---
+++ sfml_windows/main.cpp
@@ -0,0 +1,33 @@
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+//#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <windows.h>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+
+////////////////////////////////////////////////////////////
+/// Entry point of application
+///
+/// \return Application exit code
+///
+////////////////////////////////////////////////////////////
+//using namespace std;
+int main()
+{
+  sf::Music music;
+  
+  std::cout<<"playing file"<<std::endl;
+  if (!music.openFromFile("resources/file_example_OOG_2MG.ogg")){
+    std::cout<<"open from file error"<<std::endl;
+    return -1; // error
+  }
+  std::cout<<"playing file"<<std::endl;
+  music.play();
+  Sleep(10000);
+  return EXIT_SUCCESS;
+}
